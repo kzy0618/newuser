2345f5cd9b80bf0bd629b839c55f822cf56e10aa
VoNZproject <voiceofnewzealand@gmail.com>
2017-06-08 15:50:31 +1200

    changes icon app

--- appinfo/app.php
+++ appinfo/app.php
@@ -33,7 +33,7 @@ $container->query('OCP\INavigationManager')->add(function () use ($container) {
 
 		// the icon that will be shown in the navigation
 		// this file needs to exist in img/
-		'icon' => $urlGenerator->imagePath('newuser', 'app.svg'),
+		'icon' => $urlGenerator->imagePath('newuser', 'newuser.svg'),
 
 		// the title of your application. This will be used in the
 		// navigation or on the settings page of your app
